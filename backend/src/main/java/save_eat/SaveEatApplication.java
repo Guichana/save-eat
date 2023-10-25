b551dfb5e6f76013c130e17b063ed9c709fd92ba
guichana70 <guichana70@gmail.com>
2023-10-25 18:51:53 +0900

    spring boot 초기화

---
+++ backend/src/main/java/save_eat/SaveEatApplication.java
@@ -0,0 +1,13 @@
+package save_eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveEatApplication.class, args);
+	}
+
+}
